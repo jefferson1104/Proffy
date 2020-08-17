fcda807ba3d5a68ebd74436c8d6171e35efcd93a
jefferson <jefferson1104junior@gmail.com>
2020-08-17 00:06:39 -0300

    react-navigation

---
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function GiveClasses() {
+  return(
+    <>
+    <View > 
+      <Text> Aulas disponiveis </Text>
+    </View>
+    </>
+  ) 
+}
+
+export default GiveClasses;
